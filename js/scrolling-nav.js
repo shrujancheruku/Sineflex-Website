68065e4f7316a7bd1891cc7669c89a7529ba5f0a
shrujancheruku <shrujancheruku@gmail.com>
2018-05-02 00:39:22 -0500

    Scripting Updates

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -9,11 +9,17 @@ $(window).scroll(function() {
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $(document).on('click', 'a.page-scroll', function(event) {
+    $(document).on("click", "a.page-scroll", function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        $("html, body")
+            .stop()
+            .animate(
+                {
+                    scrollTop: $($anchor.attr("href")).offset().top
+                },
+                1500,
+                "easeInOutExpo"
+            );
         event.preventDefault();
     });
 });
